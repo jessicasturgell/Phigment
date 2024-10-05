fe03a9227cc9268cbb6b60c3fe3f40c2f4b1e295
Jessica Sturgell <sturgell01@gmail.com>
2024-10-04 21:11:46 -0400

    can now get complementary colors

--- Phigment/client/src/components/explore/Explore.jsx
+++ Phigment/client/src/components/explore/Explore.jsx
@@ -1,3 +1,45 @@
+import "./Explore.css";
+import chroma from "chroma-js";
+import { useEffect, useState } from "react";
+
 export const Explore = () => {
-  return <></>;
+  const [color, setColor] = useState("magenta");
+  const [complementaryColor, setComplementaryColor] = useState("magenta");
+
+  const getComplement = () => {
+    const hsl = chroma(color).hsl();
+    const complementaryHue = (hsl[0] + 180) % 360;
+    const complementary = chroma.hsl(complementaryHue, hsl[1], hsl[2]).hex();
+    setComplementaryColor(complementary);
+  };
+
+  useEffect(() => {
+    getComplement();
+  }, [color]);
+
+  return (
+    <>
+      <div className="color-container">
+        <h1>Select a color</h1>
+        <input
+          type="color"
+          defaultValue="#FF00FF"
+          onChange={(event) => {
+            setColor(event.target.value);
+          }}
+        />
+        <div
+          className="swatch"
+          style={{
+            backgroundColor: color,
+          }}
+        ></div>
+        <h1>Complementary</h1>
+        <div
+          className="swatch"
+          style={{ backgroundColor: complementaryColor }}
+        ></div>
+      </div>
+    </>
+  );
 };
