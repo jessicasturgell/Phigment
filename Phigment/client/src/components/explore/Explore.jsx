43ebbefdc10d57853a52a5559c21c61bf789b6d9
Jessica Sturgell <sturgell01@gmail.com>
2024-10-08 12:40:54 -0400

    added modal and add swatch buttons

--- Phigment/client/src/components/explore/Explore.jsx
+++ Phigment/client/src/components/explore/Explore.jsx
@@ -2,6 +2,7 @@ import "./Explore.css";
 import chroma from "chroma-js";
 import { HexColorPicker } from "react-colorful";
 import { useEffect, useState } from "react";
+import AddSwatchToPalette from "../forms/AddSwatchToPalette.jsx";
 
 export const Explore = () => {
   const [color, setColor] = useState("#ff00ff");
@@ -97,7 +98,7 @@ export const Explore = () => {
             <span
               style={{
                 backgroundColor: color,
-                color: textColor, // apca contrast implementation
+                color: textColor, // contrast implementation
               }}
             >
               {color}
@@ -114,22 +115,38 @@ export const Explore = () => {
                 backgroundColor: color,
               }}
             >
-              <span className="swatch-text">{color}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{color}</span>
+              </div>
             </div>
             <div
               className="swatch"
               style={{ backgroundColor: complementaryColor }}
             >
-              <span className="swatch-text">{complementaryColor}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{complementaryColor}</span>
+              </div>
             </div>
           </div>
+
           <h1>Analogous</h1>
           <div className="flex">
             <div
               className="swatch"
               style={{ backgroundColor: analogousColors[0] }}
             >
-              <span className="swatch-text">{analogousColors[0]}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{analogousColors[0]}</span>
+              </div>
             </div>
             <div
               className="swatch"
@@ -137,22 +154,38 @@ export const Explore = () => {
                 backgroundColor: color,
               }}
             >
-              <span className="swatch-text">{color}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{color}</span>
+              </div>
             </div>
             <div
               className="swatch"
               style={{ backgroundColor: analogousColors[1] }}
             >
-              <span className="swatch-text">{analogousColors[1]}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{analogousColors[1]}</span>
+              </div>
             </div>
           </div>
+
           <h1>Triadic</h1>
           <div className="flex">
             <div
               className="swatch"
               style={{ backgroundColor: triadicColors[0] }}
             >
-              <span className="swatch-text">{triadicColors[0]}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{triadicColors[0]}</span>
+              </div>
             </div>
             <div
               className="swatch"
@@ -160,22 +193,38 @@ export const Explore = () => {
                 backgroundColor: color,
               }}
             >
-              <span className="swatch-text">{color}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{color}</span>
+              </div>
             </div>
             <div
               className="swatch"
               style={{ backgroundColor: triadicColors[1] }}
             >
-              <span className="swatch-text">{triadicColors[1]}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{triadicColors[1]}</span>
+              </div>
             </div>
           </div>
+
           <h1>Monochromatic</h1>
           <div className="flex">
             <div
               className="swatch"
               style={{ backgroundColor: monochromaticColors[0] }}
             >
-              <span className="swatch-text">{monochromaticColors[0]}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{monochromaticColors[0]}</span>
+              </div>
             </div>
             <div
               className="swatch"
@@ -183,22 +232,42 @@ export const Explore = () => {
                 backgroundColor: monochromaticColors[1],
               }}
             >
-              <span className="swatch-text">{monochromaticColors[1]}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{monochromaticColors[1]}</span>
+              </div>
             </div>
             <div className="swatch" style={{ backgroundColor: color }}>
-              <span className="swatch-text">{color}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{color}</span>
+              </div>
             </div>
             <div
               className="swatch"
               style={{ backgroundColor: monochromaticColors[2] }}
             >
-              <span className="swatch-text">{monochromaticColors[2]}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{monochromaticColors[2]}</span>
+              </div>
             </div>
             <div
               className="swatch"
               style={{ backgroundColor: monochromaticColors[3] }}
             >
-              <span className="swatch-text">{monochromaticColors[3]}</span>
+              <div className="swatch-btn">
+                <AddSwatchToPalette />
+              </div>
+              <div>
+                <span className="swatch-text">{monochromaticColors[3]}</span>
+              </div>
             </div>
           </div>
         </div>
